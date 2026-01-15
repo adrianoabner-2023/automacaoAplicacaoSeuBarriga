d419e421c1cabb88137e19b66814da8a5820b392
Adriano Abner Venancio Barbosa <adrianoabner7@gmail.com>
2026-01-15 20:23:51 -0300

    projeto Cypress com comandos customizados

---
+++ cypress/e2e/seuBarriga.cy.js
@@ -0,0 +1,37 @@
+describe('Teste do site Seu Barriga com Comandos Customizados', () => {
+
+  beforeEach(() => {
+    // Usando o comando de login
+    cy.login('adriano.barbosa@gmail.com', '321');
+  });
+
+  it('Adicionando conta', () => {
+    cy.adicionarConta('Segunda conta Test');
+    cy.get('.alert').should('contain', 'sucesso');
+  });
+
+  it('Removendo a conta criada', () => {
+    cy.removerUltimaConta();
+    cy.get('.alert').should('contain', 'sucesso');
+  });
+
+  it('Criando movimentação', () => {
+    const movimentacao = {
+      tipo: 'Despesa',
+      dataTransacao: '14/01/2026',
+      dataPagamento: '20/01/2026',
+      descricao: 'Passeio no salão de carros',
+      interessado: 'Adriano Abner',
+      valor: '67.00'
+    };
+
+    cy.criarMovimentacao(movimentacao);
+    cy.get('.alert').should('contain', 'sucesso');
+  });
+
+  it('Verificando resumo mensal e removendo registro', () => {
+    // Remove o último registro de Janeiro/2026
+    cy.removerUltimoResumoMensal('Janeiro', '2026');
+    cy.get('.alert').should('contain', 'sucesso');
+  });
+});
---
+++ cypress/support/commands.js
@@ -0,0 +1,44 @@
+// Comando para Login
+Cypress.Commands.add('login', (email, senha) => {
+    cy.visit('https://seubarriga.wcaquino.me/');
+    cy.get('#email').type(email);
+    cy.get('#senha').type(senha);
+    cy.contains('button', 'Entrar').click();
+    cy.get('.alert-success').should('be.visible');
+});
+
+// Comando para Adicionar Conta
+Cypress.Commands.add('adicionarConta', (nomeConta) => {
+    cy.contains('a', 'Contas').click();
+    cy.contains('a', 'Adicionar').click();
+    cy.get('#nome').type(nomeConta);
+    cy.get('.btn-primary').click();
+});
+
+// Comando para Remover a Última Conta da lista
+Cypress.Commands.add('removerUltimaConta', () => {
+    cy.contains('a', 'Contas').click();
+    cy.contains('a', 'Listar').click();
+    cy.get('#tabelaContas tbody tr').last().find('a[href*="remover"]').click();
+});
+
+// Comando para Criar Movimentação
+Cypress.Commands.add('criarMovimentacao', (dados) => {
+    cy.contains('a', 'Criar Movimentação').click();
+    cy.get('[name="tipo"]').select(dados.tipo);
+    cy.get('[name="data_transacao"]').clear().type(dados.dataTransacao);
+    cy.get('[name="data_pagamento"]').clear().type(dados.dataPagamento);
+    cy.get('[name="descricao"]').type(dados.descricao);
+    cy.get('[name="interessado"]').type(dados.interessado);
+    cy.get('[name="valor"]').type(dados.valor);
+    cy.get('.btn').click();
+});
+
+// Comando para Remover Último Registro do Resumo Mensal
+Cypress.Commands.add('removerUltimoResumoMensal', (mes, ano) => {
+    cy.contains('a', 'Resumo Mensal').click();
+    cy.get('[name="mes"]').select(mes);
+    cy.get('[name="ano"]').select(ano);
+    cy.get('.btn').click();
+    cy.get('table tbody tr').last().find('a[href*="remover"]').click();
+});
